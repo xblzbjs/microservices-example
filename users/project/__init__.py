6e0760420eea258c3a43a7992e2a9a1de4ed5c9b
xblzbjs <710255295@qq.com>
2021-08-13 20:55:17 +0800

    Postgres and Docker Setup

--- users/project/__init__.py
+++ users/project/__init__.py
@@ -1,9 +1,28 @@
+import os
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 # set config
+app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object('project.config.DevelopmentConfig')
+
+# instantiate the db
+db = SQLAlchemy(app)
+
+
+# model
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 
 @app.route('/users/ping', methods=['GET'])
