2f334664ad1eca3314405a0835581292e7de9e4a
xblzbjs <710255295@qq.com>
2021-08-15 20:11:58 +0800

    Jinjia Templates Setup

--- users/project/tests/test_users.py
+++ users/project/tests/test_users.py
@@ -125,6 +125,26 @@ class TestUserService(BaseTestCase):
             self.assertIn('xblzbjs@man.org', data['data']['users'][1]['email'])
             self.assertIn('success', data['status'])
 
+    def test_main_no_users(self):
+        """Ensure the main route behaves correctly when no users have been added to the database"""
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'<h1>All Users</h1>', response.data)
+        self.assertIn(b'<p>No users!</p>', response.data)
+
+    def test_main_with_users(self):
+        """Ensure the main route behaves correctly when users have been added to the database"""
+        with self.client:
+            response = self.client.post(
+                '/',
+                data=dict(username='xblzbjs', email='xblzbjs@man.org'),
+                follow_redirects=True,
+            )
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'<h1>All Users</h1>', response.data)
+            self.assertNotIn(b'<p>No users!</p>', response.data)
+            self.assertIn(b'xblzbjs', response.data)
+
 
 if __name__ == '__main__':
     unittest.main()
