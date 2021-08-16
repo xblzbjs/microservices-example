0738412c2aa53d3c12d38235fa53a847b7786fbb
xblzbjs <710255295@qq.com>
2021-08-16 21:43:47 +0800

    Update Structure

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -6,10 +6,13 @@ class BaseConfig:
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'my_precious'
+    DEBUG_TB_ENABLED = False
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
 
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
+    DEBUG_TB_ENABLED = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
